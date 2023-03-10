0d71c7b5defede4c4cf2ed11d31ea70908baac96
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-03-10 10:34:20 -0500

    update DogCEO coding for assignment completion

--- Assignments/DogCEO2/main.js
+++ Assignments/DogCEO2/main.js
@@ -1,19 +1,15 @@
-// fetch("https://dog.ceo/api/breeds/list/all")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
 const getAllBreeds = async () => {
   const x = await fetch("https://dog.ceo/api/breeds/list/all");
-  const response = await x.json(); //is this the interpolation?
+  const response = await x.json();
   const data = response.message;
   // console.log(data);
   return data;
 };
 
 async function readBreedData() {
-  //you said you chose the async & await approach rather than the what_____approach, yesterday?
   const data = await getAllBreeds();
   // console.log(listOfBreedsObject);
-  const breeds = Object.keys(data); //this is when the breed and square bracket are created from the data? what we see in the console window...
+  const breeds = Object.keys(data);
   // console.log(breeds);
 
   let emptyArray = [];
@@ -31,4 +27,4 @@ async function readBreedData() {
   console.log(emptyArray);
 }
 
-readBreedData(); //this is calling the function? when you tell it to 'read', we are doing what...calling it?
+readBreedData();
