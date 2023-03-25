f9ac67d3e28e213a73972c29a2bb4efd2c2c2f2c
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-03-25 10:55:34 -0400

    add updates

--- Assignments/AQP.js
+++ Assignments/AQP.js
@@ -1,4 +1,6 @@
-const artwork = ["https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&limit=2&fields=id,title,image_id"];
+const artwork = [
+  "https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&limit=2&fields=id,title,image_id",
+];
 const blindArt = () => Math.random() >= 0.5;
 
 function getData() {
@@ -7,7 +9,7 @@ function getData() {
       if (blindArt()) {
         resolve(artwork);
       } else {
-        reject('There was a problem with the server, please try again.');
+        reject("There was a problem with the server, please try again.");
       }
     }, 500);
   });
@@ -16,7 +18,7 @@ function getData() {
 function getRandomIntInclusive(min, max) {
   min = Math.ceil();
   max = Math.floor();
-  return Math.floor(Math.random() * 40) + 1);
+  return Math.floor(Math.random() * 40) + 1;
 }
 
 async function getArtwork() {
