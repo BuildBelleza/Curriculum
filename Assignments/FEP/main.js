9092c838fd43aec5f03a0544aae6f020bdf3fc5d
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-04-09 21:53:16 -0400

    add code for frontend project

--- Assignments/FEP/main.js
+++ Assignments/FEP/main.js
@@ -1,41 +1,55 @@
-import "./style.css";
-
 const app = document.getElementById("app"); //we are caching the element so we can attach to the DOM
 
 let clickCount = 0;
 let firstCard = "";
 let secondCard = "";
 let cardIds = [];
+// let item = [];
 
 const picturesPick = async () => {
   const x = await fetch(
-    "https://https://www.flickr.com/services/rest/?method=flickr.photos.getPopular&api_key=f315febdc75fb1e9b82dec177517312a&user_id=197962419%40N03&per_page=10&page=1&format=json&nojsoncallback=1.ceo/api/breeds/list/all"
+    "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f315febdc75fb1e9b82dec177517312a&tags=circus&per_page=10&format=json&nojsoncallback=1"
   );
   const response = await x.json();
-  const data = response.message;
-  // console.log(data);
-  return data;
+  const data = [response.photos.photo.id];
+  console.log(data);
+  // return data;
 };
 
-[...data, ...data].forEach((item, index) => renderItem(item.name, index)); //duplicating your array and housing it into an array
-//grid of 4 rows of 5 taller than wide
+// const getPhotoId = async () => {
+//   const data = await picturesPick();
+//   const photoData = data.photo_id((item) => item.is_public_domain === true);
+//   // console.log(filteredData);
+//   return filteredData;
+// };
 
-function renderItem(name, index) {
+const getData = async () => {
+  const cardIdsArray = await picturesPick;
+  [...data, ...data].forEach((item, index) => renderItem(item.photo_id, index));
+  fullCardArray(fullGame);
+  console.log(fullGameArray);
+  return fullGameArray;
+}; //duplicating your array and housing it into an array
+// //grid of 4 rows of 5 taller than wide
+
+function renderItem(photo_id, index) {
   const div = document.createElement("div");
-  div.setAttribute("createRandomId()");
+  div.setAttribute(photo_id, `${photo_id}-${index}`);
+  // div.setAttribute("createRandomId()");
   app.appendChild(div); //entry point to the DOM
   div.classList = "card";
-  div.id = createRandomId();
+  console.log(renderItem);
+  // div.id = createRandomId();
 }
 
-//add a class to elements in the HTML
+// //add a class to elements in the HTML
 app.addEventListener("click", handleClick);
 
 function handleClick(e) {
   const el = e.target;
   const card = el.getAttribute("data-name");
-  // let isMatch = firstCard === getCardName(card);
-  // let isNotMatch = firstCard != getCardName(card);
+  let isMatch = firstCard === getCardName(card);
+  let isNotMatch = firstCard != getCardName(card);
 
   const isRevealed = el.classList.contains("revealed");
 
@@ -88,8 +102,8 @@ function checkIsMatch(val) {
   // return firstCard === val;
 }
 
-function createRandomId() {
-  return (Math.random() + 1).toString(36).substring(7);
-}
+// function createRandomId() {
+//   return (Math.random() + 1).toString(36).substring(7);
+// }
 
 // app.removeEventListener('handleClick');
