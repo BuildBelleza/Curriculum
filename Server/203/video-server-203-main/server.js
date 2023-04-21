9220b3bcfbec23d0b6e97c812dd3616456ca5c58
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-04-21 03:27:07 -0400

    add up[dated code to assignments files

--- Server/203/video-server-203-main/server.js
+++ Server/203/video-server-203-main/server.js
@@ -1,28 +1,51 @@
-const express = require('express');
-const es6Renderer = require('express-es6-template-engine');
+const express = require("express");
+const es6Renderer = require("express-es6-template-engine");
+const navs = require("./data/navs.json");
 
 const server = express();
 
 server.use(express.static(__dirname + "/public"));
 
-server.engine('html', es6Renderer);
-server.set('views', 'views');
-server.set('view engine', 'html');
+//.use is middleware and is saying root here and static is saying to use the public folder. __dirname is a global variable that is the path to the current directory. The + is saying to add the public folder to the end of the path.
 
-server.get('/', (req, res) => {
-  res.render('index', {
+server.engine("html", es6Renderer);
+server.set("views", "views");
+server.set("view engine", "html");
+
+server.get("/", (req, res) => {
+  res.render("index", {
+    locals: {
+      title: "Video Server 203",
+      navs,
+      blah: {},
+    },
     partials: {
-      footer: 'partials/footer',
-      header: 'partials/header',
-      main: 'partials/main'
-    }
+      footer: "partials/footer",
+      header: "partials/header",
+      main: "partials/main/landing",
+    },
   });
 });
 
-server.get('/heartbeat', (req, res) => {
-  res.json({"is": "working"});
+server.get("/gallery", (req, res) => {
+  res.render("index", {
+    locals: {
+      title: "Video Server 203",
+      navs,
+      blah: {},
+    },
+    partials: {
+      footer: "partials/footer",
+      header: "partials/header",
+      main: "partials/main/gallery",
+    },
+  });
+});
+
+server.get("/heartbeat", (req, res) => {
+  res.json({ is: "working" });
 });
 
 server.listen(8080, () => {
-  console.log('The server is listening at PORT 8080');
+  console.log("The server is listening at PORT 8080");
 });
