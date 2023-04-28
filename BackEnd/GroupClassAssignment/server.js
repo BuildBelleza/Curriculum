2b6a409532d7ff119dc94115c891a5442d34cc57
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-04-27 20:02:34 -0400

    add code to server.js for pgp connection

--- BackEnd/GroupClassAssignment/server.js
+++ BackEnd/GroupClassAssignment/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const pgp = require('pg-promise')();
 
 const es6Renderer = require("express-es6-template-engine");
 
 const server = express();
+
+const cn = {
+  host: 'localhost',
+  port: 5432,
+  database: 'mockcompany',
+  user: 'postgres',
+  password: 'test',
+  allowExitOnIdle: true
+};
+
+const db = pgp(cn);
 
 server.use(express.static(__dirname + "/public"));
 
